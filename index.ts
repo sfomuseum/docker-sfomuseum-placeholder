9b114ef7eb45d37f97684b9f64d2c36fd7581c43
thisisaaronland <aaron@localhost>
2021-11-22 11:33:31 -0800

    snapshot: start finessing CDK stuff a bit to handle specifics

--- index.ts
+++ index.ts
@@ -5,8 +5,11 @@ import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 import { config } from "dotenv";
 config();
 
-var nextzen = process.env.PLACEHOLDER_NEXTZEN_APIKEY || "123456";
+var nextzenApiKey = process.env.PLACEHOLDER_NEXTZEN_APIKEY || "123456";
+var placeholderStaticPrefix = process.env.PLACEHOLDER_STATIC_PREFIX || "";
 var vpcID = process.env.VPC_ID || "123456";
+var vpcIsDefault = (process.env.VPC_IS_DEFAULT == "true") ? true : false;
+var ec2InstanceType = process.env.INSTANCE_TYPE || "123456";
 
 class EcsGoPlaceholderStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps){
@@ -15,7 +18,7 @@ class EcsGoPlaceholderStack extends cdk.Stack {
         // This looks up your existing VPC by ID provided in the .env file.
         // Be sure to check if your existing VPC is a "Default VPC" or not and adjust the parameter below.
         const vpc = ec2.Vpc.fromLookup(this, 'vpc', {
-            isDefault: true,
+            isDefault: vpcIsDefault,
             vpcId: vpcID
         })
 
@@ -24,10 +27,21 @@ class EcsGoPlaceholderStack extends cdk.Stack {
         cluster.addCapacity('DefaultAutoScalingGroupCapacity', {
             // Here is where you can select the instance type you want to use and desired count for the auto-scaling-group. 
             // You might want to use something smalled, or try the Graviton2 type instance family.
-            instanceType: new ec2.InstanceType("m6i.large"),
+            instanceType: new ec2.InstanceType(ec2InstanceType),
             desiredCapacity: 1,
         });
 
+	var ec2Env = {
+                    DEPLOYED_DATE: Date.now().toLocaleString(),
+                    PLACEHOLDER_NEXTZEN_APIKEY: nextzenApiKey,
+	}
+
+	// To do: assign any/all process.env.PLACEHOLDER_ variables
+	
+	if (placeholderStaticPrefix != "") {
+	   ec2Env["PLACEHOLDER_STATIC_PREFIX"] = placeholderStaticPrefix;
+	}
+	
         const ec2Service = new ecs_patterns.ApplicationLoadBalancedEc2Service(this, "EC2Service", {
             cluster,
             // here is where you can adjust the memory limit for each task
@@ -35,10 +49,7 @@ class EcsGoPlaceholderStack extends cdk.Stack {
             taskImageOptions: {
                 image: ecs.ContainerImage.fromAsset(`${__dirname}`),
                 containerPort: 8080,
-                environment: {
-                    DEPLOYED_DATE: Date.now().toLocaleString(),
-                    PLACEHOLDER_NEXTZEN_APIKEY: nextzen
-                }
+                environment: ec2Env,
             },
             desiredCount: 1
         });
